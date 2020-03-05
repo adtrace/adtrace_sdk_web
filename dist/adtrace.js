18a1d70538660a0bbd755751941b5056d57f4a85
Aref <arefhosseini@yahoo.com>
2020-03-05 21:00:43 +0330

    update version to 1.1.0

--- dist/adtrace.js
+++ dist/adtrace.js
@@ -6,7 +6,10 @@
     var req = new XMLHttpRequest();
 
     req.open(method, url, !0);
-    req.setRequestHeader('Client-SDK', 'js1.0.0');
+    req.setRequestHeader('Client-SDK', 'js1.1.0');
+    if (method == 'POST') {
+      req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    }
     req.onreadystatechange = function () {
       if (req.readyState === 4) {
         if (req.status >= 200 && req.status < 400) {
@@ -58,22 +61,72 @@
     options = options || {};
 
     var _baseParams = cloneObj(options.device_ids);
+    var _sendSession = false;
+    var _adid = localStorage.getItem('adtrace_js_sdk_id');
 
     _baseParams.app_token = options.app_token;
     _baseParams.environment = options.environment;
     _baseParams.os_name = options.os_name || 'android';
+    _baseParams.app_version = options.app_version;
+
+    _initializeSDK();
 
     return {
+      getAdId: getAdId,
       trackSession: trackSession,
       trackEvent: trackEvent
     };
 
-    function trackSession(onSuccess, onError) {
+    function _initializeSDK() {
+
+      if (_adid == null) {
+        trackInstall(function (result) {
+          var resultJson = JSON.parse(result.responseText);
+          if (resultJson.hasOwnProperty('adid')) {
+            _adid = resultJson.adid;
+            localStorage.setItem('adtrace_js_sdk_id', _adid);
+            if (_sendSession) {
+              _sendSession = false;
+              setTimeout(function(){ trackSession(function (sessionResult) {}, function (sessionErrorMsg, sessionError) {}); }, 5000);
+            }
+          }
+        }, function (errorMsg, error) {
+        });
+      }
+
+    }
+
+    function trackInstall(onSuccess, onError) {
 
       var params = cloneObj(_baseParams);
 
-      sendRequest('GET', 'https://app.adtrace.io/session?'+  encodeQueryString(params), null, onSuccess, onError);
+      params.device_type = 'phone';
+      params.source = 'install_referrer';
+      params.referrer = 'utm_source=google-play&utm_medium=organic';
+      params.android_uuid = params.gps_adid;
+      params.needs_response_details = 1;
+      params.event_buffering_enabled = 0;
+      params.raw_referrer = params.referrer;
+      params.tracking_enabled = 1;
+      params.installed_at = (new Date().toISOString() + '+0000');
+      params.session_count = 1;
+      params.attribution_deeplink = 1;
+      params.created_at = params.installed_at;
+      params.sent_at = params.installed_at;
+        
+      sendRequest('POST', 'https://app.adtrace.io/sdk_click', encodeQueryString(params), onSuccess, onError);      
 
+    }
+
+    function trackSession(onSuccess, onError) {
+      if (_adid == null) {
+        onError(new Error('Adtrace Session will sent after inistalling'), '');
+        _sendSession = true;
+      }
+      else {
+        var params = cloneObj(_baseParams);
+        sendRequest('GET', 'https://app.adtrace.io/session?' + encodeQueryString(params), null, onSuccess, onError);
+      }
     }
 
     function trackEvent(options, onSuccess, onError) {
@@ -102,6 +155,10 @@
 
       sendRequest('GET', 'https://app.adtrace.io/event?' + encodeQueryString(params), null, onSuccess, onError)
 
+    }
+
+    function getAdId() {
+      return _adid;
     }
 
     function _getRevenue(options) {
