ef0bafc621810c17cf25c9a0346d51ca191c4f0b
namini40 <namini40@gmail.com>
2023-04-09 11:45:28 +0330

    refactor demo

--- src/demo/disable-third-party-sharing/disable-third-party-sharing.js
+++
@@ -1,8 +0,0 @@
-import Adtrace from '../../sdk/main'
-import SimpleAction from '../simple-action'
-
-function init () {
-  SimpleAction('dtps', Adtrace.disableThirdPartySharing)()
-}
-
-export default init
